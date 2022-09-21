35c316b27cb24c280dfc22f3ddd4cef84031fa73
Roberto Madinyá <roberto.madinya@ioet.com>
2022-09-20 20:35:54 -0500

    added function add product and total calculator

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,7 +1,14 @@
+import { useContext } from "react";
+import { CartContext } from "../../context/cart.context";
 import ButtonCustom from "../button/button.component";
 import "./product-card.style.scss";
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addItemHandler = () => {
+    addItemToCart(product);
+  };
 
   return (
     <div className="product-card-container">
@@ -10,7 +17,10 @@ const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <ButtonCustom buttonType="inverted"> Add to the cart</ButtonCustom>
+      <ButtonCustom buttonType="inverted" onClick={addItemHandler}>
+        {" "}
+        Add to the cart
+      </ButtonCustom>
     </div>
   );
 };
